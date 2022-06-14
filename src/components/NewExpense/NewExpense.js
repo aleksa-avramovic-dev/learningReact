7fa39d4af6befd89295bbdf92895e18619726224
aleksa-avramovic-dev <aleksa.avramovic.dev@gmail.com>
2022-06-14 16:05:32 +0200

    Checking out

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,9 +3,17 @@ import ExpenseForm from "./ExpensesForm";
 import "./NewExpense.css";
 
 const NewExpense = () => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    console.log(expenseData);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
